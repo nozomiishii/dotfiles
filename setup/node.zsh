fb444d566f8a516aa9febb37337447dbd462eb63
Nozomi Ishii <nozomiishii.jp@gmail.com>
2021-02-25 11:00:49 +0900

    feat: create node setup

--- setup/node.zsh
+++ setup/node.zsh
@@ -1,16 +1,24 @@
 #!/usr/bin/env zsh
 echo "\n🐉Starting Node Setup\n"
+# How to remove nvm
+# rm -rf $NVM_DIR ~/.npm ~/.bower
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.0/install.sh | zsh
-
-nvm install --lts
-
-echo "\n📦nvm installed\n"
-node -v
+if type node > /dev/null 2>&1 ; then
+  echo "🐉Node exists, skip install"
+  node -v
+else
+  echo "🐉Node doesn't exist, continuing with install"
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.0/install.sh | zsh
+  
+  source ~/.zshrc 
+  nvm install --lts
+  node -v 
+  echo "📦nvm installed"
+fi
 
 npm i -g typescript
 npm i -g firebase-tools
 npm i -g yarn
 
-echo "\n📦npm installed\n"
+echo "📦npm installed"
 npm list -g --depth=0
