73235b794ec3960723e10d362d91a00acb0a69f1
Nozomi Ishii <nozomiishii.jp@gmail.com>
2021-08-19 02:13:59 +0900

    ✨: add prisma shortcut

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -10,7 +10,7 @@ alias brewi="./setup/homebrew.zsh"
 alias brewclean="brew bundle cleanup --force && brew cleanup && brew upgrade"
 alias zclean="zinit self-update && zinit update && zinit delete --clean"
 alias dotclean="brewclean && zclean"
-alias yul="yarn upgrade --latest"
+alias play="chmod +x ./playground.sh && ./playground.sh"
 
 
 # Change Directory
@@ -26,12 +26,24 @@ alias vu="cd $CODE_DIR/Work/voice-utopia"
 alias vuapi="cd $CODE_DIR/Work/voice-utopia-api"
 alias vuo="cd $CODE_DIR/Work/voice-utopia-office"
 
-alias grmb="git branch --merged|egrep -v '\*|main|dev|stag|prod'|xargs git branch -d && git fetch --prune"
-alias deploy="git push origin prod && git checkout main"
-alias hbb="hub browse"
+
+# shell
 alias quit="exec $SHELL -l"
 alias zz="source ~/.zshrc"
 alias ll="exa -laFh --git"
 alias ls="exa"
+
+
+# git
+alias grmb="git branch --merged|egrep -v '\*|main|dev|stag|prod'|xargs git branch -d && git fetch --prune"
+alias deploy="git push origin prod && git checkout main"
+alias hbb="hub browse"
+
+
+# npm
+alias prisma="npx prisma"
+alias yul="yarn upgrade --latest"
+
+
+# network
 alias wifi="networksetup -setairportpower en0 off && networksetup -setairportpower en0 on"
-alias play="chmod +x ./playground.sh && ./playground.sh"
