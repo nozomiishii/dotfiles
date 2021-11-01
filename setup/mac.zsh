5d63d1d06ae2d9967ff85a47f6847cdbf8c46ac0
Nozomi Ishii <nozomiishii.jp@gmail.com>
2021-11-01 20:04:05 +0900

    ✨: key repeat for apps

--- setup/mac.zsh
+++ setup/mac.zsh
@@ -9,7 +9,7 @@ fi
 ln -nfsv "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim/init.vim"
 ln -nfsv "$HOME/dotfiles/nvim/coc-settings.json" "$HOME/.config/nvim/coc-settings.json"
 # Turn key repear on
-defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+defaults write -g ApplePressAndHoldEnabled -bool false
 # Install vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
