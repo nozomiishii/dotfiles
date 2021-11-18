c69e1294f0b3f2111b17caddab266416aaae8722
Nozomi Ishii <nozomiishii.jp@gmail.com>
2021-11-18 19:01:05 +0900

    🚀: update repo name

--- setup/code.zsh
+++ setup/code.zsh
@@ -5,4 +5,4 @@ if [ ! $CODE_DIR ]; then
   CODE_DIR="$HOME/Code"
 fi
 
-git clone https://github.com/nozomiishii/c2021 $CODE_DIR/nozomiishii/c2021
+git clone https://github.com/nozomiishii/dev $CODE_DIR/nozomiishii/dev
