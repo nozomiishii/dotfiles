0002d04c8b7adcd39112e2e784d89cda321eba57
Nozomi Ishii <nozomiishii@Nozomis-MacBook-Pro.local>
2021-12-11 22:35:19 +0900

    🗑: remove dotbot

--- setup/terraform.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-echo "\n🛰 Starting Terraform Setup\n"
-
-if ! type terraform > /dev/null 2>&1; then
-  GREP_OPTIONS="--color=never" tfenv install latest
-  source ~/.zshrc 
-  tfenv use $(echo $(tfenv list) | cut -d " " -f 1)
-  terraform --version
-fi 
