67ea776f6a30e3969a5af3859d77caf11cd05295
Nozomi Ishii <nozomiishii.jp@gmail.com>
2021-12-13 05:16:06 +0900

    🐞: docker snipets

--- modules/zsh/zinit.zsh
+++ modules/zsh/zinit.zsh
@@ -10,12 +10,24 @@ source "${ZINIT_HOME}/zinit.zsh"
 autoload -Uz _zinit
 (( ${+_comps} )) && _comps[zinit]=_zinit
 
-zinit ice depth=1; zinit light 'romkatv/powerlevel10k'
-zinit snippet 'OMZL::git.zsh'
-zinit snippet 'OMZP::git'
-zinit snippet 'OMZP::kubectl'
 
-zinit light 'g-plane/zsh-yarn-autocompletions'
-zinit light 'zsh-users/zsh-completions'
-zinit light 'zsh-users/zsh-syntax-highlighting'
-# zinit light 'asdf-vm/asdf'
+zinit snippet OMZL::git.zsh
+zinit snippet OMZP::git
+
+zinit light docker/cli
+zinit ice as"completion"
+zinit snippet OMZP::docker/_docker
+
+zinit light zsh-users/zsh-completions
+zinit light zsh-users/zsh-syntax-highlighting
+zinit light g-plane/zsh-yarn-autocompletions
+zinit light b4b4r07/enhancd
+zinit light zdharma-continuum/history-search-multi-word
+zinit light Aloxaf/fzf-tab
+
+
+autoload -Uz compinit
+compinit
+
+
+zinit ice depth=1; zinit light romkatv/powerlevel10k
--- src/docker.zsh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zsh
-echo "\n🐳 Starting Docker Setup\n"
-
-# Install shell completion
-# https://docs.docker.com/docker-for-mac/#install-shell-completion
-etc=/Applications/Docker.app/Contents/Resources/etc
-sudo ln -s $etc/docker.zsh-completion /usr/local/share/zsh/site-functions/_docker
-sudo ln -s $etc/docker-compose.zsh-completion /usr/local/share/zsh/site-functions/_docker-compose
