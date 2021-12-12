f5952b93d565c80c58555f24b362f35b42ff42e5
Nozomi Ishii <nozomiishii.jp@gmail.com>
2021-12-13 03:55:58 +0900

    🗑: android studio and pycharm setup

--- src/apps.zsh
+++ src/apps.zsh
@@ -37,57 +37,9 @@ stow -vd "$APPS_PATH" -t "$HOME/Library/Application Support/Code/User" VSCode
 echo "- 🍎 Xcode"
 stow -vd "$APPS_PATH" -t "$HOME/Library/Developer/Xcode/UserData" Xcode
 
-XCODE_USERDATA="$HOME/Library/Developer/Xcode/UserData"
-CUSTOMIZED_XCODE_USERDATA="$APP_PREFERENCES/Xcode/UserData"
 
-XCODE_USERDATA_ITEMS=("CodeSnippets" "FontAndColorThemes" "KeyBindings")
-for item in ${XCODE_USERDATA_ITEMS[@]}; do
-  # Need rm -rf to symbolic-link KeyBindings folder
-  rm -rf "$XCODE_USERDATA/$item"
-  ln -nfsv "$CUSTOMIZED_XCODE_USERDATA/$item" "$XCODE_USERDATA/$item"
-  if [ -L "$XCODE_USERDATA/$item" ]; then
-    echo "Created Link: $XCODE_USERDATA/$item"
-  else
-    echo "Error: Creating Links fails"
-  fi
-done
-ls -l "$XCODE_USERDATA"
-
-
-PYCHARM_VERSION="CE2021.2"
-echo "- 🐍 PyCharm$PYCHARM_VERSION"
-
-PYCHARM_USERDATA="$HOME/Library/Application Support/JetBrains/PyCharm$PYCHARM_VERSION"
-CUSTOMIZED_PYCHARM_USERDATA="$APP_PREFERENCES/PyCharm"
-
-rm -rf "$PYCHARM_USERDATA"
-ln -nfsv "$CUSTOMIZED_PYCHARM_USERDATA" "$PYCHARM_USERDATA"
-ls -l "$PYCHARM_USERDATA"
-
-
-
-ANDROID_STUDIO_VERSION="2020.3"
-echo "- 🐸 Android Studio $ANDROID_STUDIO_VERSION"
-
-ANDROID_STUDIO_USERDATA="$HOME/Library/Application Support/Google/AndroidStudio$ANDROID_STUDIO_VERSION"
-CUSTOMIZED_ANDROID_STUDIO_USERDATA="$APP_PREFERENCES/AndroidStudio"
-
-rm -rf "$ANDROID_STUDIO_USERDATA"
-ln -nfsv "$CUSTOMIZED_ANDROID_STUDIO_USERDATA" "$ANDROID_STUDIO_USERDATA"
-ls -l "$ANDROID_STUDIO_USERDATA"
-
-
-
-
-
-
-echo "- 🐵 Blender"
-BLENDER_USERDATA="$HOME/Library/Application Support/Blender"
-CUSTOMIZED_BLENDER_USERDATA="$APP_PREFERENCES/Blender"
-
-rm -rf "$BLENDER_USERDATA"
-ln -nfsv "$CUSTOMIZED_BLENDER_USERDATA" "$BLENDER_USERDATA"
-ls -l "$BLENDER_USERDATA"
+# echo "- 🐵 Blender"
+# stow -vd "$APPS_PATH" -t "$HOME/Library/Application Support/Blender" Blender
 
 
 echo "Automator 🤖"
