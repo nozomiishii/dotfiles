6865c44a32dc699144c84354c422d5e130237730
Nozomi Ishii <nozomiishii.jp@gmail.com>
2021-12-14 04:01:46 +0900

    🐞: autocompletion

--- modules/zsh/.zsh/zinit.zsh
+++ modules/zsh/.zsh/zinit.zsh
@@ -22,8 +22,8 @@ zinit light zsh-users/zsh-syntax-highlighting
 zinit light b4b4r07/enhancd
 zinit light zdharma-continuum/history-search-multi-word
 zinit light Aloxaf/fzf-tab
-# zinit light zsh-users/zsh-completions
-# zinit light g-plane/zsh-yarn-autocompletions
+zinit light zsh-users/zsh-completions
+zinit light g-plane/zsh-yarn-autocompletions
 
 
 autoload -Uz compinit
