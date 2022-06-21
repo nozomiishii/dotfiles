c8d9047351f6c2b8d5949d24fa21d60ea0907b8a
Nozomi Ishii <nozomiishii.jp@gmail.com>
2022-06-21 13:11:36 +0900

    ✨: format

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,20 @@
+// https://prettier.io/docs/en/options.html
 module.exports = {
-  plugins: [require("prettier-plugin-sh")],
+  arrowParens: 'always',
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  bracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 100,
+  proseWrap: 'always',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+
+  plugins: [require('prettier-plugin-sh')],
 };
