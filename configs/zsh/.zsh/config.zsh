800080448312e4b43f71c22bcc571022dd311aa5
Nozomi Ishii <nozomiishii.jp@gmail.com>
2022-10-09 20:39:06 +0900

    fix(zsh): change HISTSIZE=10000

--- configs/zsh/.zsh/config.zsh
+++ configs/zsh/.zsh/config.zsh
@@ -7,8 +7,8 @@ setopt autopushd
 
 # Command history
 HISTFILE=$HOME/.zsh_history
-HISTSIZE=50000
-SAVEHIST=50000
+HISTSIZE=10000
+SAVEHIST=10000
 
 # Do not save duplicate history
 setopt hist_ignore_all_dups
