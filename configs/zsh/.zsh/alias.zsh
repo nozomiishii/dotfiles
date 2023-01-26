2a1023e32080c316d67b03bfd3c963b28201856e
Nozomi Ishii <nozomiishii.jp@gmail.com>
2023-01-27 01:09:11 +0900

    chore: format alias.zsh

--- configs/zsh/.zsh/alias.zsh
+++ configs/zsh/.zsh/alias.zsh
@@ -1,26 +1,36 @@
 # Aliases
 
+# ----------------------------------------------------------------
 # Variables
+# ----------------------------------------------------------------
 CODE_DIR="$HOME/Code"
 GOOGLE_DRIVE_DIR="$HOME/Google\ Drive"
 
+# ----------------------------------------------------------------
 # NeoVim
+# ----------------------------------------------------------------
 alias vi="nvim"
 alias vim="nvim"
 alias view="nvim -R"
 alias vimconfig="vim $HOME/dotfiles/nvim/init.vim"
 
+# ----------------------------------------------------------------
 # Maintenance
+# ----------------------------------------------------------------
 alias brewi="./setup/homebrew.zsh"
 alias brewclean="brew bundle cleanup --verbose --file "$HOME/dotfiles/Brewfiles/macos/Brewfile" --force && brew cleanup && brew upgrade"
 alias zclean="zinit self-update && zinit update && zinit delete --clean"
 alias dotclean="brewclean && zclean"
 alias play="chmod +x ./playground.sh && ./playground.sh"
 
+# ----------------------------------------------------------------
 # Open VScode
+# ----------------------------------------------------------------
 alias fj="code ."
 
+# ----------------------------------------------------------------
 # Change Directory
+# ----------------------------------------------------------------
 alias drive="cd $GOOGLE_DRIVE_DIR"
 alias dsk="cd ~/Desktop"
 alias plist="cd ~/Library/Preferences"
@@ -34,13 +44,17 @@ alias arc="cd $CODE_DIR/nozomiishii/archives"
 alias cv="cd $CODE_DIR/nozomiishii/cv"
 alias vscode="cd $CODE_DIR/nozomiishii/.vscode"
 
+# ----------------------------------------------------------------
 # shell
+# ----------------------------------------------------------------
 alias quit="exec $SHELL -l"
 alias zz="exec $SHELL"
 alias ll="exa -laFh --git"
 alias ls="exa"
 
+# ----------------------------------------------------------------
 # git
+# ----------------------------------------------------------------
 alias grmb="git branch --merged|egrep -v '\*|master|main|dev|develop|development|stag|staging|prod|production'|xargs git branch -d && git fetch --prune"
 alias deploy="git push origin prod && git checkout main"
 alias ghb="gh browse"
@@ -51,14 +65,20 @@ alias gsmp="git submodule foreach git pull"
 alias gil="gh issue list -a nozomiishii"
 alias gilw="gh issue list -a nozomiishii -w"
 
+# ----------------------------------------------------------------
 # npm
+# ----------------------------------------------------------------
 alias prisma="npx prisma"
 alias yul="yarn upgrade --latest"
 
+# ----------------------------------------------------------------
 # network
+# ----------------------------------------------------------------
 alias wifi="networksetup -setairportpower en0 off && networksetup -setairportpower en0 on"
 
+# ----------------------------------------------------------------
 # Docker
+# ----------------------------------------------------------------
 alias dcls="docker container ls"
 alias dcr="docker container run"
 alias dcs="docker container start -a"
@@ -68,12 +88,18 @@ alias dsp="docker system prune --all --volumes"
 alias dcu='docker-compose up'
 alias dcd='docker-compose down'
 
+# ----------------------------------------------------------------
 # tfenv https://github.com/tfutils/tfenv/issues/83
+# ----------------------------------------------------------------
 alias tfenv='GREP_OPTIONS="--color=never" tfenv'
 
+# ----------------------------------------------------------------
 # terraform
+# ----------------------------------------------------------------
 alias tf="terraform"
 
+# ----------------------------------------------------------------
 # tmux
+# ----------------------------------------------------------------
 alias tt=" tmux source-file ~/.tmux.conf && tmux display '🦕: tmux reloaded'"
 alias tclean="~/.tmux/plugins/tpm/bin/clean_plugins"
