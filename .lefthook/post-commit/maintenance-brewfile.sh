8361a63bff07f6bf48a8402f59f81b1277303c2a
Nozomi Ishii <nozomiishii.jp@gmail.com>
2023-04-12 03:08:35 +0900

    chore: move remove_temp_files.sh to remove_temp_files dir

--- .lefthook/post-commit/maintenance-brewfile.sh
+++ .lefthook/post-commit/maintenance-brewfile.sh
@@ -7,8 +7,8 @@
 set -Ceu
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
-# shellcheck source=../../utils/remove_temp_files.sh
-source "$SCRIPT_DIR/../../utils/remove_temp_files.sh"
+# shellcheck source=../../utils/remove_temp_files/remove_temp_files.sh
+source "$SCRIPT_DIR/../../utils/remove_temp_files/remove_temp_files.sh"
 # shellcheck source=../../utils/request_admin_privileges.sh
 source "$SCRIPT_DIR/../../utils/request_admin_privileges.sh"
 
