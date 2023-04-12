3ee9cb7e787f77c94d8c23afe51b08b48cb5d376
Nozomi Ishii <nozomiishii.jp@gmail.com>
2023-04-13 02:15:29 +0900

    chore: separate node toolchain from toolchains.sh

---
+++ src/toolchains/node.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# -C: Prevent overwriting files with output redirection
+# -e: Exit the script if any command returns a non-zero status
+# -u: Exit the script if an undefined variable is used
+# -x: (Optional) Enable command tracing for easier debugging
+set -Ceu
+GREEN='\033[0;32m'
+NO_COLOR='\033[0m'
+
+echo -e '🐉 Node\n'
+
+echo '- 🐉 Install Node with Volta⚡️'
+curl https://get.volta.sh | bash
+export VOLTA_HOME="$HOME/.volta"
+export PATH="$VOLTA_HOME/bin:$PATH"
+
+echo "- ⚡️ volta $(volta --version)"
+
+volta install node
+volta install yarn@1
+
+echo "- 🐉 node $(node --version)"
+echo "- 🚚 yarn $(yarn --version)"
+
+echo '- 🐉 Setup corepack'
+yarn global add corepack
+corepack enable
+corepack enable npm
+
+echo -e "\n${GREEN}🐉 Node setup is complete 🎉${NO_COLOR}\n\n"
