498ad68e167ad99a2b5a1587ecab936c52933b45
Nozomi Ishii <nozomiishii.jp@gmail.com>
2023-04-19 18:31:49 +0900

    refactor(configs): use fn msg to output message

--- src/configs/configs.sh
+++ src/configs/configs.sh
@@ -5,14 +5,16 @@
 # -u: Exit the script if an undefined variable is used
 # -x: (Optional) Enable command tracing for easier debugging
 set -Ceu
-GREEN='\033[0;32m'
-RESET='\033[0m'
-
-echo -e "🧝🏻‍♀️ Starting Configs setup...\n\n"
 
 configs_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+# Including 'shellcheck source' enables Bash IDE (language server) to perform definition peeking and jumping
+# shellcheck source=../../utils/create_symlinks/create_symlinks.sh
 source "$configs_dir/../../utils/create_symlinks/create_symlinks.sh"
+# shellcheck source=../../utils/msg/msg.sh
+source "$configs_dir/../../utils/msg/msg.sh"
+
+msg --title "🧝🏻‍♀️ Starting Configs setup..."
 
 create_symlinks --source "$configs_dir" --target "$HOME"
 
-echo -e "\n\n${GREEN}🧝🏻‍♀️ Setup Configs is complete 🎉${RESET}\n\n"
+msg --success "🧝🏻‍♀️ Setup Configs is complete 🎉"
