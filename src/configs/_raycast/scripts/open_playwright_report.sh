4d6aa73d9f6c0acf7540a94665e0f3b704d3b111
Nozomi Ishii <nozomiishii.jp@gmail.com>
2023-04-26 03:52:21 +0900

    chore(raycast): add user friendly comments to open_playwright_report

--- src/configs/_raycast/scripts/open_playwright_report.sh
+++ src/configs/_raycast/scripts/open_playwright_report.sh
@@ -20,13 +20,13 @@
 # -o pipefail : Change pipeline exit status to the last non-zero exit
 #               code in the pipeline, or zero if all commands succeed
 # -x          : (Optional) Enable command tracing for easier debugging
-set -Ceuo pipefail
+set -Ceu
 
 # ----------------------------------------------------------------
 # Constants
 # ----------------------------------------------------------------
-report_zip="$HOME/Desktop/playwright-report.zip"
-port=8080
+download_path="$HOME/Desktop"
+report_zip_prefix="playwright-report"
 
 # ----------------------------------------------------------------
 # Node.js Version Manager
@@ -42,6 +42,17 @@ if [ -d "$HOME/.volta" ]; then
   export PATH="$VOLTA_HOME/bin:$PATH"
 fi
 # Add your Node.js version manager configuration below:
+#
+# -----------------------------------------------
+# START OF YOUR NODE JS VERSION MANAGER CONFIGURATION
+#
+# Please add your Node.js version manager configuration here.
+# Refer to your version manager's documentation for the correct
+# configuration settings.
+#
+# -----------------------------------------------
+# END OF YOUR NODE JS VERSION MANAGER CONFIGURATION
+#
 
 # ----------------------------------------------------------------
 # Functions
@@ -51,7 +62,7 @@ msg_error() {
   local red="\033[1;31m"
   local reset='\033[0m'
 
-  echo -e "${red}ERROR: ${message}${reset}"
+  echo -e "${red}ERROR: ${message}${reset}" >&2
 }
 
 main() {
@@ -60,17 +71,57 @@ main() {
     exit 1
   fi
 
-  if [ ! -f "$report_zip" ]; then
-    msg_error "playwright-report.zip not found on Desktop."
+  # ----------------------------------------------------------------
+  # Find Latest Report and Extract
+  # ----------------------------------------------------------------
+  # Find all files in the download directory
+  # -maxdepth 1 : Search only in the download directory, not in subdirectories
+  # -name       : Match filenames with the specified pattern
+  # -type f     : Search for files only, not directories
+  # -exec       : Execute the 'stat' command on each matched file
+  #
+  # Use 'stat' to get the modification timestamp and filename for each file
+  # -f "%m %N"  : Format the output with the modification timestamp (%m) and filename (%N)
+  #
+  # | sort -nr   : Sort the output numerically (-n) and in reverse order (-r)
+  # | head -n 1  : Get the first line of the sorted output (the latest file)
+  # | awk '{print $2}' : Extract the filename (second field) from the first line
+  local latest_report_zip
+  latest_report_zip=$(find "$download_path" -maxdepth 1 -name "${report_zip_prefix}*.zip" -type f -exec stat -f "%m %N" {} \; | sort -nr | head -n 1 | awk '{print $2}')
+
+  if [ -z "$latest_report_zip" ]; then
+    msg_error "No ${report_zip_prefix} file with timestamp found on ${download_path}"
     exit 1
   fi
 
-  local report_directory
-  report_directory="${report_zip%.zip}"
+  local report_directory="${latest_report_zip%.zip}"
+  unzip -o "$latest_report_zip" -d "$report_directory"
 
-  unzip -o "$report_zip" -d "$report_directory"
+  # ----------------------------------------------------------------
+  # Launch HTTP Server and Open Report
+  # ----------------------------------------------------------------
+  local output_file
+  output_file=$(mktemp)
 
-  npx http-server --port "$port" "$report_directory" &
-  open "http://localhost:${port}"
+  # http-server
+  # https://www.npmjs.com/package/http-server
+  #
+  # --port 0  : Use --port 0 to look for an open port, starting at 8080.
+  npx http-server --port 0 "$report_directory" 2>&1 | tee "$output_file" &
+  # Wait for the http-server output to be available
+  sleep 2
+
+  # Extract the port number from the output file
+  local assigned_port
+  assigned_port=$(grep -m1 -o -E "http:\/\/127\.0\.0\.1:([0-9]+)" "$output_file" | awk -F: '{print $NF}')
+
+  if [ -z "$assigned_port" ]; then
+    msg_error "No assigned port found"
+    rm -f "$output_file"
+    exit 1
+  fi
+
+  open "http://localhost:${assigned_port}"
+  rm -f "$output_file"
 }
 main
